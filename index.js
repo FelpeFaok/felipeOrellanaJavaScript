7b7ec508ebc54f72d5270afb72f632a3a6ea87c1
Felipe Orellana <felpe.faok@gmail.com>
2022-09-07 00:31:21 -0300

    agregar visualizacion html

--- index.js
+++ index.js
@@ -26,58 +26,116 @@
 //     }
 //     break;
 // }
+// // ingreso de datos usuario
+// let consulta1 = prompt("Desea Registrarse? ingrese 1");
+// if (consulta1 === "1"){
+//     class Usuario{
+//         constructor(nombre, email, clave){
+//             this.nombre = nombre;
+//             this.email = email;
+//             this.clave = clave;
+//         }
+//     }
+//     const usuario = [];
 
+//     for (let i = 0; i < 1; i++) {
 
+//         let nombre = prompt("Ingrese su nombre");
+//         let email = prompt("Ingrese su email");
+//         let clave = prompt("Ingrese su clave");
+//         let usuarios = new Usuario(nombre, email, clave);
+//         usuario.push(usuarios);
+//     }
+//     console.log(usuario);
+// }else{
+//     console.log("no se a registrado");
+// }
 
-// ingreso de datos usuario
+// // ingreso de datos para productos
 
+// let consulta2 = prompt("Desea agregar 5 productos? introdusca la tecla 1");
+
+// if (consulta2 === "1"){
+//     class Producto{
+//         constructor(img, precio, nombreProducto){
+//             this.img = img;
+//             this.precio = precio;
+//             this.nombreProducto = nombreProducto;
+//         }
+//     }
+//     const productos = [];
+//     for (let i = 0; i < 5; i++) {
+//         let img = prompt("Ingrese HTML de su producto");
+//         let precio = prompt("Ingrese precio de su producto");
+//         let nombre = prompt("Ingrese nombre de su producto");
+//         let card = new Producto(img, precio, nombre);
+//         productos.push(card);
+//     }
+//     console.log(productos);
+// }else{
+//     console.log("no a ingresado ningun producto");
+// }
+
+// crear usuario dentro de "cardUsuario" y visualizarlo
+let ingreso = document.getElementById("usuario");
+class Usuario {
+  constructor(nombre, email, clave) {
+    this.nombre = nombre;
+    this.email = email;
+    this.clave = clave;
+  }
+}
+const cardUsuarios = [];
 let consulta1 = prompt("Desea Registrarse? ingrese 1");
+if (consulta1 === "1") {
+  for (let i = 0; i < 1; i++) {
+    let nombre = prompt("Ingrese su nombre");
+    let email = prompt("Ingrese su email");
+    let clave = prompt("Ingrese su clave");
+    let usuario = new Usuario(nombre, email, clave);
+    cardUsuarios.push(usuario);
+  }
+} else {
+  alert("no se a registrado");
+}
+for (const tarjeta of cardUsuarios) {
+  let presentacion = document.createElement("div");
+  presentacion.innerHTML = `<h2>Nombre: ${tarjeta.nombre}</h2>
+                      <p>Mail: ${tarjeta.email}</p>`;
 
-if (consulta1 === "1"){
-    class Usuario{
-        constructor(nombre, email, clave){
-            this.nombre = nombre;
-            this.email = email;
-            this.clave = clave;       
-        }
-    }
-    const usuario = [];
-
-    for (let i = 0; i < 1; i++) {
-
-        let nombre = prompt("Ingrese su nombre");
-        let email = prompt("Ingrese su email");
-        let clave = prompt("Ingrese su clave");
-        let usuarios = new Usuario(nombre, email, clave);
-        usuario.push(usuarios);
-    }
-    console.log(usuario);
-}else{
-    console.log("no se a registrado");
+  ingreso.append(presentacion);
 }
 
-// ingreso de datos para productos
-
-let consulta2 = prompt("Desea agregar 5 productos? introdusca la tecla 1");
-
-if (consulta2 === "1"){
-    class Producto{
-        constructor(img, precio, nombreProducto){
-            this.img = img;
-            this.precio = precio;
-            this.nombreProducto = nombreProducto;       
-        }
-    }
-    const productos = [];
-    for (let i = 0; i < 5; i++) {
-        let img = prompt("Ingrese HTML de su producto");
-        let precio = prompt("Ingrese precio de su producto");
-        let nombre = prompt("Ingrese nombre de su producto");
-        let card = new Producto(img, precio, nombre);
-        productos.push(card);
-    }
-    console.log(productos);
-}else{
-    console.log("no a ingresado ningun producto");
+// crear elementos dentro de "card" y visualizarlo
+let contenedor = document.getElementById("contenedor");
+let numProducto = prompt("Ingrese cantidad de productos a ingresar");
+class Producto {
+  constructor(id, nombre, url, precio) {
+    this.id = id;
+    this.nombre = nombre;
+    this.url = url;
+    this.precio = precio;
+  }
 }
 
+const productos = [];
+for (let i = 0; i < numProducto; i++) {
+  let id = prompt("Ingrese id de su producto");
+  let nombre = prompt("Ingrese nombre de su producto");
+  let url = prompt("Ingrese url de la imagen del producto");
+  let precio = prompt("Ingrese precio de su producto");
+  let card = new Producto(id, nombre, url, precio);
+  productos.push(card);
+}
+
+console.log(productos);
+
+for (const cards of productos) {
+  let item = document.createElement("div");
+  item.innerHTML = `<h2>ID: ${cards.id}</h2>
+                      <p>Producto: ${cards.nombre}</p>
+                      <img src="${cards.url}"/>
+                      <b>$${cards.precio}</b>`;
+
+  contenedor.append(item);
+}
